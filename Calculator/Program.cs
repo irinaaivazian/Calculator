08d8521eb68b2b2c9e18e6c43ef313f546ebf87f
Irina <Irina@LAPTOP-P147OHQP>
2024-06-01 01:10:58 +0400

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.WriteLine("Please choose the operation: (+, -, *, /)");
+                string function = Console.ReadLine();
+
+                if (function == "+")
+                {
+
+                    Console.Write("Please write the first number: ");
+                    decimal a = (decimal.Parse(Console.ReadLine()));
+                    Console.Write("Please write the second number: ");
+                    decimal b = decimal.Parse(Console.ReadLine());
+                    decimal c = a + b;
+                    Console.WriteLine($"The answer is: {c}");
+                }
+                else if (function == "-")
+                {
+
+                    Console.Write("Please write the first number: ");
+                    decimal a = (decimal.Parse(Console.ReadLine()));
+                    Console.Write("Please write the second number: ");
+                    decimal b = decimal.Parse(Console.ReadLine());
+                    decimal c = a - b;
+                    Console.WriteLine($"The answer is: {c}");
+                }
+                else if (function == "*")
+                {
+
+                    Console.Write("Please write the first number: ");
+                    decimal a = (decimal.Parse(Console.ReadLine()));
+                    Console.Write("Please write the second number: ");
+                    decimal b = decimal.Parse(Console.ReadLine());
+                    decimal c = a * b;
+                    Console.WriteLine($"The answer is: {c}");
+                }
+                else if (function == "/")
+                {
+
+                    Console.Write("Please write the first number: ");
+                    decimal a = (decimal.Parse(Console.ReadLine()));
+                    Console.Write("Please write the second number: ");
+                    decimal b = decimal.Parse(Console.ReadLine());
+
+                    if (b != 0)
+                    {
+                        decimal c = a / b;
+                        Console.WriteLine($"The answer is: = {c}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("The number can't be devided to 0.");
+                    };
+                }
+                else
+                {
+                    Console.WriteLine("Please write only these symbols (+, -, *, /).");
+                };
+        }
+    }
+}
---
+++ Calculator2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculator2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****CALCULATOR*****");
+        }
+    }
+}
